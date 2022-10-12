31bccee68da31e3efec28b5f1151bac5265e3ea2
salman <najarsalman4@gmail.com>
2022-10-12 12:39:20 +0530

    header footer created indivaidually

---
+++ header.php
@@ -0,0 +1,51 @@
+ <!--================Header Menu Area =================-->
+ <header class="header_area">
+        <div class="top_menu row m0 bgColor ">
+            <div class="container">
+                <div class="float-left">
+                    <a class="dn_btn text-white" href="mailto:medical@example.com"><i class="ti-email"></i>medical@example.com</a>
+                </div>
+                <div class="float-right">
+                    <ul class="list header_social ">
+                        <li><a href="#"><i class="ti-facebook text-white"></i></a></li>
+                        <li><a href="#"><i class="ti-twitter-alt text-white"></i></a></li>
+                        <li><a href="#"><i class="ti-linkedin text-white"></i></a></li>
+                        <li><a href="#"><i class="ti-skype text-white"></i></a></li>
+                        <li><a href="#"><i class="ti-vimeo-alt text-white"></i></a></li>
+                    </ul>	
+                </div>
+            </div>	
+        </div>	
+        <div class="main_menu">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="container">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <a class="navbar-brand logo_h w-5" href="index.php"><img class="imageLogo" src="img/images.png" alt=""></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+                        <ul class="nav navbar-nav menu_nav ml-auto">
+                            <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li> 
+                            <li class="nav-item"><a class="nav-link" href="about-us.php">About</a></li> 
+                            <li class="nav-item"><a class="nav-link" href="department.php">Department</a></li> 
+                            <li class="nav-item"><a class="nav-link" href="doctors.php">Doctors</a></li>    
+                            <li class="nav-item submenu dropdown">
+                                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Blog</a>
+                                <ul class="dropdown-menu">
+                                    <li class="nav-item"><a class="nav-link" href="blog.php">Blog</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="single-blog.php">Blog Details</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="element.php">element</a></li>
+                                </ul>
+                            </li> 
+                            <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <!--================Header Menu Area =================-->
